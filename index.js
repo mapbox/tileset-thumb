03bfe9029b3d748effee00e524be67860ae93140
Young Hahn <yfhahn@gmail.com>
2018-01-04 05:28:17 -0500

    Split functionality out for unit testing

--- index.js
+++ index.js
@@ -3,71 +3,97 @@ const multipipe = require('multipipe');
 const Transform = require('stream').Transform;
 const PNG = require('pngjs').PNG;
 const concat = require('concat-stream');
+const COLOR_WHITE = 255;
+const COLOR_GREY = 128;
+
+// Precache zoom level sizes
+const ZSIZE = {};
+for (let z = 0; z < 30; z++) ZSIZE[z] = Math.pow(2, z);
+
+const zxyToTile = () => {
+  return split2((line) => {
+    const zxy = line.split('/');
+    zxy[0] = parseInt(zxy[0], 10);
+    zxy[1] = parseInt(zxy[1], 10);
+    zxy[2] = parseInt(zxy[2], 10);
+    if (isNaN(zxy[0]) || isNaN(zxy[1]) || isNaN(zxy[2])) throw new Error(`Invalid zxy: ${line}`);
+    return { z: zxy[0], x: zxy[1], y: zxy[2] };
+  });
+};
+
+const paint = (buffer, width, x, y, color) => {
+  const offset = (width * y + x) * 4;
+  buffer[offset] =
+  buffer[offset + 1] =
+  buffer[offset + 2] = color;
+};
+
+// Some zoom levels have smaller dimensions (in tiles) than there are
+// pixels in the thumbnail. In these cases we'll want to paint an area
+// of the image rather than just a single pixel - calculate the size
+// of the area to paint and then iterate over each pixel in it to paint.
+const paintTile = (buffer, width, z, x, y, color) => {
+  const blockSize = Math.max(1, Math.ceil(width / ZSIZE[z]));
+  const x0 = Math.round((x / ZSIZE[z]) * width);
+  const y0 = Math.round((y / ZSIZE[z]) * width);
+  for (let x1 = 0; x1 < blockSize; x1++) {
+    for (let y1 = 0; y1 < blockSize; y1++) {
+      paint(buffer, width, x0 + x1, y0 + y1, color);
+    }
+  }
+};
+
+const createGrid = (thumbs, size, cols) => {
+  const zooms = Object.keys(thumbs).sort();
+  const minzoom = Math.min.apply(Math, zooms);
+  const maxzoom = Math.max.apply(Math, zooms);
+  const rows = Math.floor(maxzoom/cols) - Math.floor(minzoom/cols);
+  const grid = (new Uint8Array((cols * size) * (rows * size) * 4)).fill(COLOR_WHITE);
+  zooms.forEach((z, num) => {
+    const row = Math.floor(num / cols);
+    const col = num % cols;
+    for (let x = 0; x < size; x++) {
+      for (let y = 0; y < size; y++) {
+        const reader = (size * y + x) * 4
+        const wx = (size * col) + x;
+        const wy = (size * row) + y;
+        paint(grid, size * cols, wx, wy, thumbs[z][reader]);
+      }
+    }
+  });
+  return {
+    buffer: grid,
+    width: size * cols,
+    height: size * rows
+  };
+};
 
 const createThumb = (options) => {
-  options.width = options.width || 256;
-  options.height = options.height || 256;
-  options.columns = options.columns || 4;
+  // thumbnail size in pixels
+  const size = options.size || 256;
 
+  // number of thumbnails across
+  const cols = options.columns || 4;
+
+  // manage thumbnail buffers across zoom levels
   const thumbs = {};
-  const dimensions = {};
 
   const thumb = new Transform({
     objectMode: true,
     transform(tile, encoding, callback) {
-      if (!thumbs[tile.z]) {
-        const bitmap = new Uint8Array(options.width * options.height * 4);
-        bitmap.fill(255);
-        thumbs[tile.z] = bitmap;
-        dimensions[tile.z] = Math.pow(2, tile.z);
-      }
-      const blockSize = Math.max(1, Math.ceil(options.width/dimensions[tile.z]));
-      const x0 = Math.round((tile.x / dimensions[tile.z]) * options.width);
-      const y0 = Math.round((tile.y / dimensions[tile.z]) * options.height);
-      for (let x1 = 0; x1 < blockSize; x1++) {
-        for (let y1 = 0; y1 < blockSize; y1++) {
-          const offset = (options.width * (y0 + y1) + (x0 + x1)) * 4
-          thumbs[tile.z][offset] =
-          thumbs[tile.z][offset + 1] =
-          thumbs[tile.z][offset + 2] = Math.max(0, thumbs[tile.z][offset] - 128);
-        }
-      }
-      return callback();
+      thumbs[tile.z] = thumbs[tile.z] || (new Uint8Array(size * size * 4)).fill(COLOR_WHITE);
+      paintTile(thumbs[tile.z], size, tile.z, tile.x, tile.y, COLOR_GREY);
+      callback();
     },
     flush(callback) {
-      const zooms = Object.keys(thumbs).sort();
-      const minzoom = Math.min.apply(Math, zooms);
-      const maxzoom = Math.max.apply(Math, zooms);
-      const cols = options.columns;
-      const rows = Math.floor(maxzoom/cols) - Math.floor(minzoom/cols);
-
-      // create multigrid
-      const grid = new Uint8Array((cols * options.width) * (rows * options.height) * 4);
-      grid.fill(255);
-
-      zooms.forEach((z, num) => {
-        const row = Math.floor(num / cols);
-        const col = num % cols;
-        for (let x = 0; x < options.width; x++) {
-          for (let y = 0; y < options.height; y++) {
-            const reader = (options.width * y + x) * 4
-            const wx = (options.width * col) + x;
-            const wy = (options.height * row) + y;
-            const writer = (options.width * cols * wy + wx) * 4;
-            grid[writer] = thumbs[z][reader];
-            grid[writer + 1] = thumbs[z][reader + 1];
-            grid[writer + 2] = thumbs[z][reader + 2];
-          }
-        }
-      });
-
       const stream = this;
+      const grid = createGrid(thumbs, size, cols);
       png = new PNG({
-        width: options.width * cols,
-        height: options.height * rows,
+        width: grid.width,
+        height: grid.height,
         bitDepth: 8
       });
-      png.data = grid;
+      png.data = grid.buffer;
       png.pack()
         .on('error', callback)
         .pipe(concat((buffer) => {
@@ -76,19 +102,12 @@ const createThumb = (options) => {
         }));
     }
   });
+
   if (options.zxy) {
-    const zxy = split2((line) => {
-      const zxy = line.split('/');
-      zxy[0] = parseInt(zxy[0], 10);
-      zxy[1] = parseInt(zxy[1], 10);
-      zxy[2] = parseInt(zxy[2], 10);
-      if (isNaN(zxy[0]) || isNaN(zxy[1]) || isNaN(zxy[2])) throw new Error(`Invalid zxy: ${line}`);
-      return { z: zxy[0], x: zxy[1], y: zxy[2] };
-    });
-    return multipipe(zxy, thumb);
+    return multipipe(zxyToTile(), thumb);
   } else {
     return thumb;
   }
 };
 
-module.exports = { createThumb };
+module.exports = { zxyToTile, paint, paintTile, createGrid, createThumb };
--- test/index.test.js
+++ test/index.test.js
@@ -2,13 +2,34 @@ const tape = require('tape');
 const createThumb = require('../index.js').createThumb;
 const fs = require('fs');
 
-tape('createThumb', (assert) => {
-  const thumb = createThumb({ zxy: true });
-  const zxy = fs.createReadStream(`${__dirname}/fixtures/tiles.zxy`);
-  zxy.pipe(thumb)
+tape('createThumb, zxy:true', (assert) => {
+  fs.createReadStream(`${__dirname}/fixtures/tiles.zxy`)
+    .pipe(createThumb({ zxy: true }))
     .on('data', (buffer) => {
       if (process.env.UPDATE) {
-        fs.writeFileSync(`${__dirname}/fixtures/tiles.expected.png`, buffer);
+        fs.writeFileSync(`${__dirname}/fixtures/tiles.256.png`, buffer);
+      }
+      assert.end();
+    });
+});
+
+tape('createThumb, zxy:true, size:128', (assert) => {
+  fs.createReadStream(`${__dirname}/fixtures/tiles.zxy`)
+    .pipe(createThumb({ zxy: true, size: 128 }))
+    .on('data', (buffer) => {
+      if (process.env.UPDATE) {
+        fs.writeFileSync(`${__dirname}/fixtures/tiles.128.png`, buffer);
+      }
+      assert.end();
+    });
+});
+
+tape('createThumb, zxy:true, size:512', (assert) => {
+  fs.createReadStream(`${__dirname}/fixtures/tiles.zxy`)
+    .pipe(createThumb({ zxy: true, size: 512 }))
+    .on('data', (buffer) => {
+      if (process.env.UPDATE) {
+        fs.writeFileSync(`${__dirname}/fixtures/tiles.512.png`, buffer);
       }
       assert.end();
     });
